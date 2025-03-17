a69f082f37f9235948131dc2d341c500d16e5ab2
piyush8512 <prajapatipiyush851@gmail.com>
2025-03-17 17:11:30 +0530

    changes fina3l

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -133,7 +133,7 @@ function Hero() {
       <p className='text-center font-libreBodoni text-xl py-8 text-[#D72327] w-2/3 mx-auto'>Weaving Stories of Tradition, Transformation, and Triumph!</p>
       </div>
       <Button variant={"destructive"} className='z-[100] fixed -left-10 top-1/2 -rotate-90 md:hidden font-leckerli'>Register Now</Button>
-      <div className='md:hidden hero-temple-mobile bg-center bg-cover bg-no-repeat h-70 mt-10 z-2 '></div>
+      <div className='md:hidden hero-temple-mobile bg-center bg-cover bg-no-repeat h-80  z-2 '></div>
       <div className='bg-[#D72327] h-40 w-full absolute bottom-0 -z-20'></div>
     </div>
   )
