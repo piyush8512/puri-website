e2a519ed5e315ee486b52fa140768505903ba1bc
psbvision <punitsinghbisht1529@gmail.com>
2025-03-17 16:50:53 +0530

    hero for mobile and programms almost fit

--- components/Programes.tsx
+++ components/Programes.tsx
@@ -3,7 +3,8 @@ import { Button } from "./ui/button";
 const Programes = () => {
   return (
     <div className="hidden md:flex min-h-[70vh] relative w-full flex-col md:flex-row md:justify-end items-end md:items-center py-16 md:py-0">
-      <div className="programs-bg bg-right bg-no-repeat bg-cover absolute left-0 -bottom-133 h-[134vh] w-full md:w-4xl -z-50"></div>
+      {/* <div className="programs-bg bg-right bg-no-repeat bg-cover absolute left-0 -bottom-133 h-[134vh] w-full md:w-4xl -z-50"></div> */}
+      <div className="programs-bg bg-center bg-no-repeat bg-cover -z-50 h-224 w-135 absolute left-10 -bottom-53"></div>
       
       <div className="w-1/2 md:w-2/5 md:mx-25 flex flex-col items-end md:items-center gap-5 px-4 md:px-5 mt-12 mb-44 md:mt-0">
         <h1 className="text-[#D72327] mt-12 pb-8 text-4xl md:text-5xl font-leckerli text-center">Programmes</h1>
