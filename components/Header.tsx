b05d4db8da9d57e61f7523a6c6b98848ca269f42
piyush8512 <prajapatipiyush851@gmail.com>
2025-04-02 22:41:26 +0530

    test2

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,6 @@ import { Button } from "@/components/ui/button";
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaLinkedinIn,
-  FaYoutube,
-  FaXTwitter,
-} from "react-icons/fa6";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
